b11b4ca018c3070327ef8d80c2c02a50156cdb53
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-09-23 05:22:43 +0000

    Add a travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,26 +1,26 @@
 #!/bin/bash -xve
 
 #required packages
-pip install catkin_pkg
-pip install empy
-pip install pyyaml
-pip install rospkg
+# In general, sudo should not be used for pip,
+# but I have written it for solving a path problem on Travis CI.
+sudo pip install catkin_pkg
+sudo pip install empy
+sudo pip install pyyaml
+sudo pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-cd ./ros_setup_scripts_Ubuntu14.04_server
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu16.04_server.git
+cd ./ros_setup_scripts_Ubuntu16.04_server
 bash ./step0.bash
 bash ./step1.bash
+
+sudo apt-get install ros-kinetic-tf ros-kinetic-tf2-geometry-msgs
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
-
-# Copyright 2016 Ryuichi Ueda
-# Released under the BSD License.
-# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
