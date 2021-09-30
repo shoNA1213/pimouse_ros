e3eb389fd62a9911e2d90f5d7123df24de8df211
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-09-30 07:21:06 +0000

    add a travis_test_lightsensors.py

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-#encording: utf8
+#encoding: utf8
 import unittest, rostest
 import rosnode, rospy
 import time
 from pimouse_ros.msg import LightSensorValues
-
+        
 class LightsensorTest(unittest.TestCase):
     def setUp(self):
         self.count = 0
         rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
         self.values = LightSensorValues()
-
+        
     def callback(self,data):
         self.count += 1
         self.values = data
@@ -26,25 +26,25 @@ class LightsensorTest(unittest.TestCase):
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()
-        self.assertIn('/lightsensors',nodes,"node does not exist")
+        self.assertIn('/lightsensors',nodes, "node does not exist")
 
     def test_get_value(self):
-        rospy.set_param('lightsensors_freq',10)
-        time.sleep(2)
-        with open("/dev/rtlightsensor0","w") as f:
+        rospy.set_param('lightsensors_freq',10)    #センサの値取得の周期を10Hzに
+        time.sleep(2)                              #パラメータの反映を待つ
+        with open("/dev/rtlightsensor0","w") as f: #ダミーの値をダミーのファイルに
             f.write("-1 0 123 4321\n")
 
         time.sleep(3)
+        ###コールバック関数が最低1回は呼ばれ、値が取得できているかを確認###
+        self.assertFalse(self.count == 0,"cannot subscribe the topic") 
+        self.check_values(4321,123,0,-1) 
 
-        self.assertFalse(self.count == 0,"cannot subscribe the topic")
-        self.check_values(4321,123,0,-1)
-
-    def test_chenge_parameter(self):
-        rospy.set_param('lightsensors_freq',1)
-        time.sleep(2)
-        c_prev = self.count
-        time.sleep(3)
-
+    def test_change_parameter(self):
+        rospy.set_param('lightsensors_freq',1)    #センサの値取得の周期を1Hzに
+        time.sleep(2)                             #反映を待つ
+        c_prev = self.count                       #callbackが呼ばれた回数を記録
+        time.sleep(3) 
+        ###コールバック関数が3秒間で最低1回、最高でも4回しか呼ばれてないことを確認###
         self.assertTrue(self.count < c_prev + 4,"freq does not change")
         self.assertFalse(self.count == c_prev,"subscriber is stopped")
 
@@ -52,3 +52,7 @@ if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_lightsensors')
     rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
+
+# Copyright 2016 Ryuichi Ueda
+# Released under the BSD License.
+# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
