a833c3fb04cafa4d6b6d616c8de4f76ea96c1f0c
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-10-06 03:58:19 +0000

    changed travis_test_lightsensors

--- test/travis_test_lightsensors.py
+++ test/travis_test_lightsensors.py
@@ -49,7 +49,7 @@ class LightsensorTest(unittest.TestCase):
         self.assertFalse(self.count == c_prev,"subscriber is stopped")
 
 if __name__ == '__main__':
-    time.sleep(3)
+    #time.sleep(3)
     rospy.init_node('travis_test_lightsensors')
     rostest.rosrun('pimouse_ros','travis_test_lightsensors',LightsensorTest)
 
