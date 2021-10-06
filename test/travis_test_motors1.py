1421eddbe2bb71c5acff346ba6e70008edbad077
Tomoki Shoda <k8111exw@sit.ac.jp>
2021-10-06 04:04:46 +0000

    changed testfiles

--- test/travis_test_motors1.py
+++ test/travis_test_motors1.py
@@ -44,7 +44,7 @@ class MotorTest(unittest.TestCase):
         self.file_check("rtmotor_raw_l0",0,"don't stop after 1[s]")
 
 if __name__ == '__main__':
-    time.sleep(3)
+    #time.sleep(3)
     rospy.init_node('travis_test_motors')
     rostest.rosrun('pimouse_ros','travis_test_motors', MotorTest)
 
